81e187bb9f69c4e23174036f4ddf63c81c955681
dachibox <dachi.gogotchuri@gmail.com>
2016-06-30 23:00:46 +0200

    Add UnitTest methods for MeetupBoards, MeetupEvent and MeetupApi.

---
+++ src/Meetup.Api.UnitTests/Services/Intern/MeetupBoardsTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using NUnit.Framework;
+
+namespace Meetup.Api.UnitTests.Services.Intern
+{
+    [TestFixture]
+    public class MeetupBoardsTest
+    {
+        [Test]
+        public void All_NoUrlNameProvided_ShouldThrowArgumentException()
+        {
+            Assert.That(async () => await MeetupApi.Boards.All("", CancellationToken.None), Throws.ArgumentException);
+        }
+    }
+}
