b6ac15f755db02a8937fe27bc87dba219a513360
dachibox <dachi.gogotchuri@gmail.com>
2016-07-03 14:13:15 +0200

    Updated MeetupBoardTests on UnitTest project.

--- src/Meetup.Api.UnitTests/Services/Intern/MeetupBoardsTest.cs
+++ src/Meetup.Api.UnitTests/Services/Intern/MeetupBoardsTest.cs
@@ -11,5 +11,35 @@ namespace Meetup.Api.UnitTests.Services.Intern
         {
             Assert.That(async () => await MeetupApi.Boards.All("", CancellationToken.None), Throws.ArgumentException);
         }
+
+        [Test]
+        public void Discussions_NoUrlNameProvided_ShouldThrowArgumentException()
+        {
+            Assert.That(async () => await MeetupApi.Boards.Discussions("", "20671181", CancellationToken.None), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void Discussions_NoBidProvided_ShouldThrowArgumentException()
+        {
+            Assert.That(async () => await MeetupApi.Boards.Discussions("CrossDevelopment-Madrid", "", CancellationToken.None), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void Discussion_NoUrlNameProvided_ShouldThrowArgumentException()
+        {
+            Assert.That(async () => await MeetupApi.Boards.Discussion("", "20671181", "49893227", CancellationToken.None), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void Discussion_NoBidProvided_ShouldThrowArgumentException()
+        {
+            Assert.That(async () => await MeetupApi.Boards.Discussion("CrossDevelopment-Madrid", "", "49893227", CancellationToken.None), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void Discussion_NoDidProvided_ShouldThrowArgumentException()
+        {
+            Assert.That(async () => await MeetupApi.Boards.Discussion("CrossDevelopment-Madrid", "20671181", "", CancellationToken.None), Throws.ArgumentException);
+        }
     }
 }
