0764862d9cc3303773f3aba078ed1e3259146ef4
dachibox <dachi.gogotchuri@gmail.com>
2016-07-07 17:56:17 +0200

    Add Create method of MeetupEvent service.

--- src/Meetup.Api/Services/Intern/MeetupEvent.cs
+++ src/Meetup.Api/Services/Intern/MeetupEvent.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
@@ -121,7 +123,7 @@ namespace Meetup.Api
         ///     Ops! Something went wrong :S. Please try again, if the error persist contact
         ///     with the developer to fix the issue.
         /// </exception>
-        public async Task<Event> ByIdAsync([NotNull]string urlName, [NotNull]string id, CancellationToken cancellationToken)
+        public async Task<Event> Event([NotNull]string urlName, [NotNull]string id, CancellationToken cancellationToken)
         {
             if (string.IsNullOrEmpty(urlName)) throw new ArgumentException("Argument is null or empty", nameof(urlName));
             if (string.IsNullOrEmpty(id)) throw new ArgumentException("Argument is null or empty", nameof(id));
@@ -138,5 +140,158 @@ namespace Meetup.Api
                 throw new HttpRequestException(Resources.ErrorMessage);
             return response;
         }
+
+        public async Task<Events> Create([NotNull]string groupUrl, [NotNull]string name, CreateEventModel content, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrEmpty(groupUrl)) throw new ArgumentException("Argument is null or empty", nameof(groupUrl));
+            if (string.IsNullOrWhiteSpace(groupUrl)) throw new ArgumentException("Argument is null or whitespace", nameof(groupUrl));
+            if (string.IsNullOrEmpty(name)) throw new ArgumentException("Argument is null or empty", nameof(name));
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Argument is null or whitespace", nameof(name));
+
+            var queryUrl = new StringBuilder(MeetupBase.BASE_URL);
+
+            var hosts = content.Hosts[0];
+            for (var i = 1; i < content.Hosts.Length; i++)
+            {
+                hosts += $",{content.Hosts[i]}";
+            }
+
+            var htmlContent = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("description", content.Description), 
+                new KeyValuePair<string, string>("duration", content.Duration.ToString()), 
+                new KeyValuePair<string, string>("guest_limit", content.GuestLimit.ToString()), 
+                new KeyValuePair<string, string>("hosts", content.Hosts.ToString()), 
+                new KeyValuePair<string, string>("how_to_find_us", content.HowToFindUs), 
+                new KeyValuePair<string, string>("publish_status", content.PublishStatus.ToString()), 
+                //new KeyValuePair<string, string>("question_1", ), 
+                new KeyValuePair<string, string>("rsvp_close", content.RSVPClose.ToString()), 
+                new KeyValuePair<string, string>("rsvp_limit", content.RSVPLimit.ToString()), 
+                new KeyValuePair<string, string>("rsvp_open", content.RSVPOpen.ToString()), 
+                new KeyValuePair<string, string>("simple_html_description", content.SimpleHtmlDescription), 
+                new KeyValuePair<string, string>("time", content.Time.ToString()), 
+                new KeyValuePair<string, string>("venue_id", content.VenueId), 
+                new KeyValuePair<string, string>("venue_visibility", content.VenueVisibility.ToString()), 
+                new KeyValuePair<string, string>("waitlisting", content.Waitlisting.ToString()), 
+                new KeyValuePair<string, string>("why", content.Why)
+            });
+
+#if DEBUG
+            queryUrl.Append($"/2/event?{SecretKeys.ApiKeyUrl}&group_urlname={groupUrl}");
+#else
+            queryUrl.Append($"/2/event?&group_urlname={groupUrl}");
+#endif
+            for (int i = 0; i < content.Questions.Length; i++)
+            {
+                queryUrl.Append($"&question{i}={content.Questions[i]}");
+            }
+
+            var response =
+                await MeetupBase.ExecuteQueryAsync<Events>(queryUrl, cancellationToken, htmlContent, HttpMethodTypes.POST);
+
+            if (response == null)
+                throw new HttpRequestException(Resources.ErrorMessage);
+            return response;
+        }
+    }
+
+    public class CreateEventModel
+    {
+        /// <summary>
+        /// Longer description of the event, in HTML. May not be longer than 50000 characters.
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Event duration in milliseconds. When not specified, a default of 3 hours may be assumed by applications. 
+        /// To clear event duration, set this to 0
+        /// </summary>
+        public double Duration { get; set; }
+        /// <summary>
+        /// Number of guests members may include in their RSVP, 0 or more
+        /// </summary>
+        public int GuestLimit { get; set; }
+        /// <summary>
+        /// Up to 5 comma-separated valid member ids to be hosts for the event. 
+        /// If hosts is not provided, the authorized member is the default host
+        /// </summary>
+        public string[] Hosts { get; set; }
+        /// <summary>
+        /// The information provided by the event host for "How will members find you there?". 
+        /// Visible when location is visible to the authenticated member
+        /// </summary>
+        public string HowToFindUs { get; set; }
+        /// <summary>
+        /// If you are an organizer of the group, you may set this to "draft" to save the event as a draft. 
+        /// Doing so will require a status=draft filter on /2/event queries.
+        /// </summary>
+        public PublishStatus PublishStatus { get; set; }
+        /// <summary>
+        /// Those with permission may include up to 6 survey questions for the event with each being up to 250 characters. 
+        /// See the parameter notes section for more information
+        /// </summary>
+        public string[] Questions { get; set; }
+        /// <summary>
+        /// Users with permission may set the RSVP close time for the event. 
+        /// The time may be specified in milliseconds since the epoch, or relative to the current time in the d/w/m format.
+        /// </summary>
+        public double RSVPClose { get; set; }
+        /// <summary>
+        /// Total number of RSVPs available for the event
+        /// </summary>
+        public int RSVPLimit { get; set; }
+        /// <summary>
+        /// Users with permission may set the RSVP open time for the event. 
+        /// The time may be specified in milliseconds since the epoch, or relative to the current time in the d/w/m format.
+        /// </summary>
+        public double RSVPOpen { get; set; }
+        /// <summary>
+        /// Description of the event, in simple HTML format. This value is translated to HTML to update the description. 
+        /// May not be longer than 50000 characters.
+        /// </summary>
+        public string SimpleHtmlDescription { get; set; }
+        /// <summary>
+        /// Event start time in milliseconds since the epoch, or relative to the current time in the d/w/m format.
+        /// </summary>
+        public double Time { get; set; }
+        /// <summary>
+        /// Numeric identifier of a venue
+        /// </summary>
+        public string VenueId { get; set; }
+        /// <summary>
+        /// Controls the visibility of the event venue for non members of the hosting group. 
+        /// May be one of "public" or "members"
+        /// </summary>
+        public VenueVisibility VenueVisibility { get; set; }
+        /// <summary>
+        /// Waiting list status may be one of: auto, manual, off
+        /// </summary>
+        public Waitlisting Waitlisting { get; set; }
+        /// <summary>
+        /// We should do this because... May not be longer than 250 characters.
+        /// </summary>
+        public string Why { get; set; }
+    }
+
+    public enum Waitlisting
+    {
+        auto,
+        manual,
+        off
+    }
+
+    public enum VenueVisibility
+    {
+        @public,
+        members
+    }
+
+    public enum PublishStatus
+    {
+        upcoming,
+        past,
+        proposed,
+        suggested,
+        cancelled,
+        draft
     }
 }
