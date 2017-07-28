3734c385a375fa3520373b41862125474b129d4a
dachibox <dachi.gogotchuri@gmail.com>
2017-07-28 15:57:08 +0200

    Add empty secret key

---
+++ src/Meetup.Api/SecretKeys.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Meetup.Api
+{
+	internal static class SecretKeys
+	{
+		internal const string ApiKey = "";
+		internal static readonly string ApiKeyUrl = $"key={ApiKey}&sign=true";
+	}
+}
